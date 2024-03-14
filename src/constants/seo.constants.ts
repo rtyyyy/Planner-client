c323d535a42ef4efa928880e8d8ac35784241c4f
rtyyyy <1etudiant@mail.ru>
2024-03-14 13:06:27 +0300

    tanstack integrated, url config, setup made

--- src/constants/seo.constants.ts
+++ src/constants/seo.constants.ts
@@ -1,2 +1,2 @@
-export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } } //закрываем страничку под сео
 export const SITE_NAME = 'Planner'
