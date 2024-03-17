9d0ff764dd5c96c1a8dc55e9bd5b83f12ed3c5d0
rtyyyy <1etudiant@mail.ru>
2024-03-17 20:19:12 +0300

    auth/reg form, ui components

---
+++ src/app/i/page.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from 'next'
+
+import { Heading } from '@/components/ui/Heading'
+
+import { NO_INDEX_PAGE } from '@/constants/seo.constants'
+
+
+
+export const metadata: Metadata = {
+	title: 'Dashboard',
+	...NO_INDEX_PAGE
+}
+
+export default function DashboardPage() {
+	return (
+		<div>
+			rrrr
+			
+		</div>
+	)
+}
