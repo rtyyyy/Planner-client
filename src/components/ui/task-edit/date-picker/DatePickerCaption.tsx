83b7c969f08842b6b83f20508f72025dc21cb407
rtyyyy <1etudiant@mail.ru>
2024-04-09 22:40:09 +0300

    datePicker ui made, singleSelect ui  made

---
+++ src/components/ui/task-edit/date-picker/DatePickerCaption.tsx
@@ -0,0 +1,35 @@
+import dayjs from 'dayjs'
+import type { DateFormatter } from 'react-day-picker'
+
+const seasonEmoji: Record<string, string> = {
+	winter: '⛄️',
+	spring: '🌸',
+	summer: '🌻',
+	autumn: '🍂'
+}
+
+const getSeason = (month: Date): keyof typeof seasonEmoji => {
+	const monthNumber = month.getMonth() + 1
+
+	if (monthNumber > 2 && monthNumber < 6) return 'spring'
+	if (monthNumber > 5 && monthNumber < 9) return 'summer'
+	if (monthNumber > 8 && monthNumber < 12) return 'autumn'
+	else return 'winter'
+}
+
+export const formatCaption: DateFormatter = month => {
+	const season = getSeason(month)
+
+	return (
+		<>
+			<span
+				role='img'
+				aria-label={season}
+				className='mr-2'
+			>
+				{seasonEmoji[season]}
+			</span>
+			{dayjs(month).format('MMMM')}
+		</>
+	)
+}
